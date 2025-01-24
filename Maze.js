942b8daab8704ae95dbd4e39a5c1954972f0d9f5
bla-bla500 <hermannc125@gmail.com>
2025-01-24 08:34:42 -0500

    Roll to green

--- Maze.js
+++ Maze.js
@@ -3,23 +3,26 @@
 //Program: Maze
 //Hardware: Bolt #SB-EC76
 
+
 //Begin Pragram
   async function startProgram() {
+
 
 //Start & Say Start
 //Bolt will say start across LED's & say it outloud
 //Driver: Aiden
 //Navigator: Caleb
 
-  await scrollMatrixText('start',{r:0,g:255,b:0},10,true);
+	await scrollMatrixText('start',{r:0,g:255,b:0},10,true);
 	await speak('Start', true);
+
 
 //Green Checkpoint
 //LED's turn Green
 //Driver: Caleb
 //Navigator: Aiden
 
-  
+	await roll(0, 50, 2.5);
 
 //Shound #1 Checkpoint
 //Bolt makes sound of choice - Update Sound
@@ -38,15 +41,18 @@
 //Driver: Caleb
 //Navigator: Aiden
 
+
 //Purple Checkpoint
 //LED's turn Purple
 //Driver: Aiden
 //Navigator: Caleb
 
+
 //Shound #3 Checkpoint
 //Bolt makes sound of choice - Update Sound
 //Driver: Caleb
 //Navigator: Aiden
+
 
 //Blue Checkpoint
 //LED's turn Purple
@@ -58,6 +64,7 @@
 //Bolt will say Finished across LED's & say it out loud
 //Driver: Caleb
 //Navigator: Aiden
+
 
 //End Program
 }
