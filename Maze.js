4d6f05c3ebec4bc9f13f1af83f5aa4d6983092ec
bla-bla500 <hermannc125@gmail.com>
2025-02-10 07:48:06 -0500

    Blue checkpoint & Fin

--- Maze.js
+++ Maze.js
@@ -32,11 +32,11 @@
 //Driver: Aiden
 //Navigator: Caleb
 
-  await roll(90, 53, 2);
-  await delay (1.5);
-  await roll (180, 40, 2);
-  await delay (1.5);
-  await Sound.Animal.Monkey.play(true);
+ await roll(90, 53, 2);
+ await delay (1.5);
+ await roll (180, 40, 2);
+ await delay (1.5);
+ await Sound.Animal.Monkey.play(true);
 
 
 //Red Checkpoint
@@ -44,11 +44,11 @@
 //Driver: Caleb
 //Navigator: Aiden
 
-  await roll(215, 41, 2);
-  await delay (1.5);
-  await roll(125, 28, 2);
-  await delay (1.5);
-  await setMainLed({r:255, g:0, b:0})
+ await roll(215, 41, 2);
+ await delay (1.5);
+ await roll(125, 29, 2);
+ await delay (1.5);
+ await setMainLed({r:255, g:0, b:0})
 
 
 //Sound #2 Checkpoint
@@ -56,30 +56,31 @@
 //Driver: Aiden
 //Navigator: Caleb
 
-  await roll(35, 41, 2);
-  await delay(1.5);
-  await Sound.Animal.Monkey.play(true);
+ await roll(35, 41, 2);
+ await delay(1.5);
+ await Sound.Animal.Monkey.play(true);
 
 //Purple Checkpoint
 //LED's turn Purple
 //Driver: Caleb
 //Navigator: Aiden
 
-  await roll(125, 36, 2);
-  await delay (1.5);
-  await roll(180, 25, 2);
-  await setMainLed({r:255, g:0, b:255})
-  await delay (1.5);
+ await roll(125, 37, 2);
+ await delay (1.5);
+ await roll(180, 26, 2);
+ await setMainLed({r:255, g:0, b:255})
+ await delay (1.5);
+
 
 //Sound #3 Checkpoint
 //Bolt makes sound of choice - Update Sound
 //Driver: Aiden
 //Navigator: Caleb
 
-  await roll (90, 31, 2);
+  await roll (90, 33, 2);
   await delay (1.5);
-  await roll(0, 31, 2)
-  await delay(1.5)
+  await roll(0, 36, 2);
+  await delay(1.5);
   await Sound.Animal.Monkey.play(true);
 
 //Blue Checkpoint
@@ -87,18 +88,18 @@
 //Driver: Caleb
 //Navigator: Aiden
 
-  await roll (-45, 36, 2);
-  await delay (1.5);
-  await setMainLed({r:0, g:0, b:255})
-  await roll (0, 255, 2);
+   await roll (-45, 40, 2);
+   await delay (1.5);
+   await setMainLed({r:0, g:0, b:255})
+   await roll (0, 255, 1);
 
 //Finished & Say Finish
 //Bolt will say Finished across LED's & say it out loud
 //Driver: Aiden
 //Navigator: Caleb
+ await scrollMatrixText('Fin',{r:255,g:0,b:0},10,true);
+ await speak('Finished', true);
 
-  await scrollMatrixText('Fin',{r:255,g:0,b:0},20,true);
-  await speak('Finished', true);
 
 //End Program
 }
